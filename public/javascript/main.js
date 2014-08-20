5c1746d90376eb943107b46b54870c574920a40d
Eli White <github@eli-white.com>
2014-08-20 12:31:40 +0200

    Making the page fixed width, adding a border between features

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,12 +1,25 @@
 function init() {
-  document.body.addEventListener("scroll", (function(e) {
+  /*document.addEventListener("scroll", (function(e) {
+    console.log("scrolling");
+
     var dist = document.body.scrollTop;
 
-    // document.queryElements("")
-    // for (var i = 0; i < )
+    var features = document.querySelectorAll(".feature");
 
-    // map.style[Platform.transform] = "translateY(-" + dist / 3 + "px)";
+    for (var i = 0; i < features.length; i++) {
+      var feature = features[i];
+      var images = feature.getElementsByTagName("img");
+
+      var offset = (dist - feature.offsetTop);
+      for (var j = 0; j < images.length; j++) {
+
+        images[j].style.transform = "translateY(" + offset / 10 + "px)";
+      }
+
+    }
+
   }).bind(this));
+*/
 }
 
 if (document.readyState === "interactive" || document.readyState === "complete") {
